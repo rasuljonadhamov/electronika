20c7d0cce3fc01fe5028ffc5900c5221960ef4fb
rasuljon <rasuljonadxamov1212@gmail.com>
2024-04-28 23:23:26 +0500

    #

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,13 +14,18 @@ const Home = () => {
     <Slider {...settings}>
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row items-center lg:justify-between">
+          <h1 className="text-black text-3xl sm:text-4xl lg:text-5xl max-w-[450px] lg:hidden font-medium mb-8 lg:mb-12 mx-9 pt-10">
+            <span className="text-[#E61212]">Aksiyada</span> ishtirok etib{" "}
+            <span className="text-primary mr-2"> Spark</span>
+            avtomobilini yutib oling!
+          </h1>
           <img
             src="/public/images/svg/spark.svg"
             alt="Slide 1"
             className="object-cover w-full lg:w-1/2 mb-8 lg:mb-0"
           />
           <div className="text-center lg:text-left lg:w-1/2">
-            <h1 className="text-black text-3xl lg:text-5xl font-medium mb-8 lg:mb-12">
+            <h1 className="text-black text-3xl xsm:hidden md:hidden lg:block hidden  lg:text-5xl font-medium mb-8 lg:mb-12">
               <span className="text-[#E61212]">Aksiyada</span> ishtirok etib{" "}
               <span className="text-primary mr-2"> Spark</span>
               avtomobilini yutib oling!
@@ -33,13 +38,18 @@ const Home = () => {
       </div>
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row items-center lg:justify-between">
+          <h1 className="text-black text-3xl lg:text-5xl sm:text-4xl max-w-[450px] lg:hidden font-medium mb-8 lg:mb-12 mx-9 pt-10">
+            <span className="text-[#E61212]">Aksiyada</span> ishtirok etib{" "}
+            <span className="text-primary mr-2"> Spark</span>
+            avtomobilini yutib oling!
+          </h1>
           <img
             src="/public/images/svg/spark.svg"
             alt="Slide 1"
             className="object-cover w-full lg:w-1/2 mb-8 lg:mb-0"
           />
           <div className="text-center lg:text-left lg:w-1/2">
-            <h1 className="text-black text-3xl lg:text-5xl font-medium mb-8 lg:mb-12">
+            <h1 className="text-black text-3xl xsm:hidden md:hidden lg:block hidden  lg:text-5xl font-medium mb-8 lg:mb-12">
               <span className="text-[#E61212]">Aksiyada</span> ishtirok etib{" "}
               <span className="text-primary mr-2"> Spark</span>
               avtomobilini yutib oling!
